be9966096a4d2125ee6efd8909f71bfd3453b666
Charles Joaquin <charlesansbert.joaquin.cics@ust.edu.ph>
2022-12-12 03:36:47 +0800

    Update customer/checkout.php
    Add errors

--- customer/checkout.php
+++ customer/checkout.php
@@ -16,15 +16,93 @@
             $name = $row['CUS_NAME'];
             $address = $row['CUS_ADDRESS'];
             $contactNumber = $row['CUS_CONTACT_NO'];
+
+            $name = $address = $contact = '';
+            $errors = array('name' => '', 'address' => '', 'contact' => '');
+
+            if (isset($_POST["place-order"]))   {
+
+                // check name
+                if (empty($_POST['name'])) {
+                    $errors['name'] = 'Name is required <br />';
+                } else {
+                    $name = $_POST['name'];
+                    if (ctype_alpha(str_replace(' ', '', $name)) === false){
+                        $errors['name'] = 'Name must be letters and spaces only';
+                    }
+                }
+
+                // check address
+                if (empty($_POST['address'])) {
+                    $errors['address'] = 'Address is required <br />';
+                } else {
+                    $address = $_POST['address'];
+                    if (!preg_match("`(.+)`s", $address)){
+                        $errors['address'] = 'Address invalid';
+                    }
+                }
+
+                // check contact
+                if (empty($_POST['contact'])) {
+                    $errors['contact'] = 'Contact is required <br />';
+                } else {
+                    $contact = $_POST['contact'];
+                    if (preg_match("/[^0-9]/", $contact)){
+                        $errors['contact'] = 'Contact invalid';
+                    }
+                }
+
+                if (array_filter($errors)) {
+                    //echo 'there are errors in the form'
+                } else {
+                    // Gets user information from session and database
+                    session_start();
+                    $userID = unserialize($_SESSION['userID']);
+
+                    $sql = "SELECT * FROM customer WHERE CUS_ID=\"".$userID."\"";
+                    $result = $conn -> query($sql);
+                    $row = $result->fetch_assoc();
+
+                    $name = $_POST['name'];
+                    $address = $_POST['address'];
+                    $contactNumber = $_POST['contact'];
+                    $totalCost = $_POST["cost"];
+                    $orderDetails = $_POST["order-details"];
+
+                    $sql = "INSERT INTO cus_order (CUS_ID, CUS_ADDRESS, CUS_NAME, CUS_CONTACT_NO, ORDER_COST, ORDER_DETAILS)
+                    VALUES ("
+                        .$userID.","
+                        ."'".$address."'".","
+                        ."'".$name."'".","
+                        .$contactNumber.","
+                        .$totalCost.","
+                        ."\"".$orderDetails."\""
+                        .")";
+
+                    if ($conn->query($sql) === TRUE) {
+                        // go to customer homepage
+                        header("Location: http://localhost/food-delivery-system/customer/index.php");
+                        exit();
+                    }
+                    else {
+                        echo "Error: " . $sql . "<br>" . $conn->error;
+                    }
+
+                    $conn -> close();
+                }
+            }
         ?>
 
         <form method="post">
             <label>Your Name:</label>
             <input type="text" name="name" value="<?php echo $name; ?>">
+            <div class="red-text"><?php echo $errors['name'] ?></div>
             <label>Address</label>
             <input type="text" name="address" value="<?php echo $address; ?>">
+            <div class="red-text"><?php echo $errors['address'] ?></div>
             <label>Contact</label>
             <input type="text" name="contact" value="<?php echo $contactNumber; ?>">
+            <div class="red-text"><?php echo $errors['contact'] ?></div>
 
             <br>
             <table>
@@ -73,44 +151,5 @@
                 <input type="submit" name="place-order" value="Place Order" class="btn brand z-depth-0">
             </div>
         </form>
-
-        <?php
-            if (isset($_POST["place-order"]))   {
-                // Gets user information from session and database
-                session_start();
-                $userID = unserialize($_SESSION['userID']);
-
-                $sql = "SELECT * FROM customer WHERE CUS_ID=\"".$userID."\"";
-                $result = $conn -> query($sql);
-                $row = $result->fetch_assoc();
-
-                $name = $_POST['name'];
-                $address = $_POST['address'];
-                $contactNumber = $_POST['contact'];
-                $totalCost = $_POST["cost"];
-                $orderDetails = $_POST["order-details"];
-
-                $sql = "INSERT INTO cus_order (CUS_ID, CUS_ADDRESS, CUS_NAME, CUS_CONTACT_NO, ORDER_COST, ORDER_DETAILS)
-                VALUES ("
-                    .$userID.","
-                    ."'".$address."'".","
-                    ."'".$name."'".","
-                    .$contactNumber.","
-                    .$totalCost.","
-                    ."\"".$orderDetails."\""
-                    .")";
-
-                if ($conn->query($sql) === TRUE) {
-                    // go to customer homepage
-                    header("Location: http://localhost/food-delivery-system/customer/index.php");
-                    exit();
-                }
-                else {
-                    echo "Error: " . $sql . "<br>" . $conn->error;
-                }
-
-                $conn -> close();
-            }
-        ?>
     <?php include 'Temps/footer.php'; ?>
 </html>
