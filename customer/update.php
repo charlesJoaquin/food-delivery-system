67ee8e53b12ab52610773bcfcaebfa8ce00fbdd8
Charles Joaquin <charlesansbert.joaquin.cics@ust.edu.ph>
2022-12-12 03:27:36 +0800

    Update format

--- customer/update.php
+++ customer/update.php
@@ -5,55 +5,55 @@
     $row= mysqli_fetch_array($result);
 
     $name = $address = $contact = '';
-        $errors = array('name' => '', 'address' => '', 'contact' => '');
-    
+    $errors = array('name' => '', 'address' => '', 'contact' => '');
 
-        if(isset($_POST['submit'])) {
 
-            // check name
-            if (empty($_POST['name'])) {
-                $errors['name'] = 'Name is required <br />';
-            } else {
-                $name = $_POST['name'];
-                if (ctype_alpha(str_replace(' ', '', $name)) === false){
-                    $errors['name'] = 'Name must be letters and spaces only';
-                }
-            }
+    if(isset($_POST['submit'])) {
 
-            // check address
-            if (empty($_POST['address'])) {
-                $errors['address'] = 'Address is required <br />';
-            } else {
-                $address = $_POST['address'];
-                if (!preg_match("`(.+)`s", $address)){
-                    $errors['address'] = 'Address invalid';
-                }
-            }
-
-            // check contact
-            if (empty($_POST['contact'])) {
-                $errors['contact'] = 'Contact is required <br />';
-            } else {
-                $contact = $_POST['contact'];
-                if (preg_match("/[^0-9]/", $contact)){
-                    $errors['contact'] = 'Contact invalid';
-                }
-            }
-
-            if (array_filter($errors)) {
-                //echo 'there are errors in the form'
-            } else {
-                echo "UPDATE cus_order set ORDER_ID=" . $_POST['id'] . ", CUS_NAME='" . $_POST['name'] . "', CUS_ADDRESS='" . $_POST['address'] . "' , CUS_CONTACT_NO=" . $_POST['contact'] . " WHERE ORDER_ID=" . $_POST['id'];
-
-                $sql = "UPDATE cus_order set ORDER_ID=" . $_POST['id'] . ", CUS_NAME='" . $_POST['name'] . "', CUS_ADDRESS='" . $_POST['address'] . "' , CUS_CONTACT_NO=" . $_POST['contact'] . " WHERE ORDER_ID=" . $_POST['id'];
-                mysqli_query($conn, $sql);
-                $message = "Record Modified Successfully";
-
-                // go to employee homepage
-                header("Location: http://localhost/food-delivery-system/customer/index.php");
-                exit();
+        // check name
+        if (empty($_POST['name'])) {
+            $errors['name'] = 'Name is required <br />';
+        } else {
+            $name = $_POST['name'];
+            if (ctype_alpha(str_replace(' ', '', $name)) === false){
+                $errors['name'] = 'Name must be letters and spaces only';
             }
         }
+
+        // check address
+        if (empty($_POST['address'])) {
+            $errors['address'] = 'Address is required <br />';
+        } else {
+            $address = $_POST['address'];
+            if (!preg_match("`(.+)`s", $address)){
+                $errors['address'] = 'Address invalid';
+            }
+        }
+
+        // check contact
+        if (empty($_POST['contact'])) {
+            $errors['contact'] = 'Contact is required <br />';
+        } else {
+            $contact = $_POST['contact'];
+            if (preg_match("/[^0-9]/", $contact)){
+                $errors['contact'] = 'Contact invalid';
+            }
+        }
+
+        if (array_filter($errors)) {
+            //echo 'there are errors in the form'
+        } else {
+            echo "UPDATE cus_order set ORDER_ID=" . $_POST['id'] . ", CUS_NAME='" . $_POST['name'] . "', CUS_ADDRESS='" . $_POST['address'] . "' , CUS_CONTACT_NO=" . $_POST['contact'] . " WHERE ORDER_ID=" . $_POST['id'];
+
+            $sql = "UPDATE cus_order set ORDER_ID=" . $_POST['id'] . ", CUS_NAME='" . $_POST['name'] . "', CUS_ADDRESS='" . $_POST['address'] . "' , CUS_CONTACT_NO=" . $_POST['contact'] . " WHERE ORDER_ID=" . $_POST['id'];
+            mysqli_query($conn, $sql);
+            $message = "Record Modified Successfully";
+
+            // go to employee homepage
+            header("Location: http://localhost/food-delivery-system/customer/index.php");
+            exit();
+        }
+    }
 ?>
 
 <html>
